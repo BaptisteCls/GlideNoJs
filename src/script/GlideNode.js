e0c38d747b0ebe53be5f3a6c4d001e3d4ad2f3b3
BaptisteCls <baptiste.collesson@gmail.com>
2024-09-23 18:04:22 +0200

    manage modules

--- src/script/GlideNode.js
+++ src/script/GlideNode.js
@@ -5,7 +5,6 @@ const configurableOptions = [
     "focusAt",
     "gap",
     "autoplay",
-    "autoplay",
     "hoverpause",
     "keyboard",
     "bound",
@@ -25,24 +24,48 @@ const configurableOptions = [
     "cloningRatio",
 ]
 
+const customOptions = {
+    autoplay : "Autoplay",
+    arrows : "Controls",
+    swipe : "Swipe",
+    keyboard : "Keyboard",
+}
+
+function createElementFromHTML(htmlString) {
+    let div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+    return div.firstChild;
+}
+
 export default class GlideNode {
+    static glideNumber = 0;
+    #glideClass;
+    #options;
+    #modules;
+
     constructor(element) {
-        this.name = element;
-        element.classList.add("glide");
-        const slides = this.#initSlides();
-        slides.append(element.childNodes);
-        element.innerHTML = '';
-        element.appendChild(slides);
+        this.glideElement = element;
+
+        this.#glideClass = `glide-${GlideNode.glideNumber++}`;
+        element.classList.add(this.#glideClass);
+        element.classList.add('glide')
+
+        const track = this.#initTrack();
+        element.appendChild(track);
+
+        this.#initOptions();
+    }
+
+    getGlideClass() {
+        return this.#glideClass;
     }
 
     getOptions() {
-        const options = {};
-        Object.keys(configurableOptions).forEach( option => {
-            const htmlOption = `glide-${option.replace(/[A-Z]/g, str => '-' + str.toLowerCase())}`;
-            const value = glideElement.getAttribute(htmlOption);
-            if (value) options[option] = value;
-        })
-        return options;
+        return this.#options;
+    }
+
+    getCustomModules() {
+        return this.#modules;
     }
 
     static getAllNodes() {
@@ -56,8 +79,11 @@ export default class GlideNode {
 
     #initSlides () {
         const slides = document.createElement('div');
-        slides.classList.add('glide_slides');
-        slides.appendChild(this.#initTrack);
+        slides.classList.add('glide__slides');
+        while (this.glideElement.childElementCount > 0) {
+            this.glideElement.firstElementChild.classList.add('glide__slide');
+            slides.appendChild(this.glideElement.firstElementChild);
+        }
         return slides;
     }
 
@@ -65,6 +91,45 @@ export default class GlideNode {
         const track = document.createElement('div');
         track.classList.add('glide__track');
         track.setAttribute('data-glide-el', 'track');
+        track.appendChild(this.#initSlides());
         return track;
+    }
+
+    #initModules = {
+        Controls : () => {
+            const arrows = createElementFromHTML(`
+                <div class="glide__arrows" data-glide-el="controls">
+                    <button class="glide__arrow glide__arrow--left" data-glide-dir="<">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM271 135c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-87 87 87 87c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L167 273c-9.4-9.4-9.4-24.6 0-33.9L271 135z"/></svg>
+                    </button>
+                    <button class="glide__arrow glide__arrow--right" data-glide-dir=">">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM241 377c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l87-87-87-87c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0L345 239c9.4 9.4 9.4 24.6 0 33.9L241 377z"/></svg>
+                    </button>
+                </div>
+            `);
+            this.glideElement.appendChild(arrows);
+        },
+    }
+
+    #initOptions () {
+        const options = {};
+        configurableOptions.forEach( option=> {
+            const htmlOption = `glide-${option.replace(/[A-Z]/g, str => '-' + str.toLowerCase())}`;
+            const value = this.glideElement.getAttribute(htmlOption);
+            if (value) options[option] = value;
+        });
+        const modules = [];
+        for (const [option, module] of Object.entries(customOptions)) {
+            const htmlOption = `glide-${option.replace(/[A-Z]/g, str => '-' + str.toLowerCase())}`;
+            const value = this.glideElement.getAttribute(htmlOption);
+            if (value && value.toLowerCase() === 'true') {
+                console.log(this.#initModules['Controls'])
+                if(this.#initModules[module])
+                    this.#initModules[module]();
+                modules.push(module);
+            }
+        };
+        this.#options = options;
+        this.#modules = modules;
     }
 }
--- src/script/glide-no.js
+++ src/script/glide-no.js
@@ -1,7 +1,20 @@
 import GlideNode from "./GlideNode.js";
-import Glide from "../base/glide.modular.esm.js"
+import Glide from "../base/glide.modular.esm.js";
+import * as Modules from "../base/glide.modular.esm.js";
 
-GlideNode.getAllNodes.forEach(element => {
-    const slider = new Glide(element, element.getOptions());
-    slider.mount();
+function getAllUsefullModules (names) {
+    const result = {};
+    for(const [name, module] of Object.entries(Modules)) {
+        if (names.includes(name))
+            result.name=module;
+    }
+    return result;
+}
+
+GlideNode.getAllNodes().forEach(element => {
+    //console.log("." + element.getGlideClass())
+    //console.log(element.getOptions())
+    const slider = new Glide("." + element.getGlideClass(), element.getOptions());
+    const modules = getAllUsefullModules(element.getCustomModules());
+    slider.mount(modules);
 });
