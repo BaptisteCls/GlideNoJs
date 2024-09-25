a01cf4607c317b3dc453fbadcf7513b7ca61afc1
BaptisteCls <baptiste.collesson.etu@univ-lille.fr>
2024-09-25 16:58:50 +0200

    Babel & Webpack environement

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/script/index.js',
+    output: {
+        filename: 'bundle.min.js',
+        path: path.resolve(__dirname, 'build')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    }
+};
